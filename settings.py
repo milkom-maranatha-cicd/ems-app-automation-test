ba78fa300190d699a197ac694b744dadd0f3f003
panjiyudasetya <panjiyudasetya@gmail.com>
2023-12-02 01:59:36 +0700

    Update project settings - Add single page application URL

--- settings.py
+++ settings.py
@@ -1,10 +1,15 @@
 from enum import IntEnum
 
 
+# Choices of the Automation Web Driver
 class DriverType(IntEnum):
     CHROME = 1
     FIREFOX = 2
     SAFARI = 3
 
 
+# Selected Web Driver
 WEB_DRIVER_TYPE = DriverType.CHROME
+
+# URL of the Single Page Application
+APP_URL = 'http://localhost:3000/'
