d9c4767cf9d192e78c17892cdbdddb6d0463a0a1
panjiyudasetya <panjiyudasetya@gmail.com>
2023-12-03 22:53:08 +0700

    Add web utilities to access located elements when visible

--- web/utils.py
+++ web/utils.py
@@ -3,7 +3,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from typing import Union
+from typing import List, Union
 
 
 def visibility_of_element_located(
@@ -21,6 +21,26 @@ def visibility_of_element_located(
     try:
         return WebDriverWait(driver, wait_for)\
             .until(EC.visibility_of_element_located((by, value)))
+    except Exception as ex:
+        driver.quit()
+        raise ex
+
+
+def visibility_of_all_elements_located(
+    driver: Union[
+        webdriver.Chrome, webdriver.Firefox, webdriver.Safari
+    ],
+    by: By,
+    value: str,
+    wait_for: int = 10,
+) -> List[WebElement]:
+    """
+    Wait until visibility of all elements with specific identifier
+    are located within that `driver`.
+    """
+    try:
+        return WebDriverWait(driver, wait_for)\
+            .until(EC.visibility_of_all_elements_located((by, value)))
     except Exception as ex:
         driver.quit()
         raise ex
