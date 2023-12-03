88137fd26de9b7af6193e564a9bef875919ebc93
panjiyudasetya <panjiyudasetya@gmail.com>
2023-12-04 01:22:49 +0700

    Reduce initial dataset for tests

--- tests/data/employees.py
+++ tests/data/employees.py
@@ -71,37 +71,5 @@ PREDEFINED_DATA = _convert_salaries([
         'email': 'laurena@example.com',
         'salary': '120000',
         'date': '2019-07-30'
-    },
-    {
-        'no': '7',
-        'first_name': 'George',
-        'last_name': 'Tallman',
-        'email': 'george@example.com',
-        'salary': '90000',
-        'date': '2019-08-15'
-    },
-    {
-        'no': '8',
-        'first_name': 'Jesica',
-        'last_name': 'Watlington',
-        'email': 'jesica@example.com',
-        'salary': '60000',
-        'date': '2019-10-10'
-    },
-    {
-        'no': '9',
-        'first_name': 'Matthew',
-        'last_name': 'Warren',
-        'email': 'matthew@example.com',
-        'salary': '71000',
-        'date': '2019-10-15'
-    },
-    {
-        'no': '10',
-        'first_name': 'Lyndsey',
-        'last_name': 'Follette',
-        'email': 'lyndsey@example.com',
-        'salary': '110000',
-        'date': '2020-01-15'
     }
 ])
