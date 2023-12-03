2626f0af3b121e8be418aeb375280d894da2dd3d
panjiyudasetya <panjiyudasetya@gmail.com>
2023-12-03 23:29:32 +0700

    Modify test scenario 001 - Complete automation test for login, validates employee dataset, and logout from the system

--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -1,9 +1,15 @@
+import time
 
 from unittest import TestCase
 
-from settings import APP_URL
 from web.driver import WebDriver
-from web.pages import SignInPage
+from web.pages import (
+    DashboardPage,
+    SignInPage,
+)
+
+from settings import APP_URL
+from tests.data.employees import PREDEFINED_DATA
 
 
 class TestScenarios(TestCase):
@@ -21,17 +27,66 @@ class TestScenarios(TestCase):
         """
         Automation testing scenario for:
         - Login with valid credentials,
-        - Fetch a list of employees, and
+        - Validates employee dataset, and
         - Logout from the system.
         """
+        # Open the sign in page
         sign_in_page = SignInPage(self.wd)
 
+        # Assert properties of the sign in page
+        # i.e. (page title, labels, and text buttons)
         try:
             sign_in_page.assert_properties()
         except ValueError as err:
             self.fail(f'Sign In Page Assertion Failed: {str(err)}')
 
-        sign_in_page.run_login('admin@mail.com', 'Test@12345')
+        # Login with valid credentials
+        dashboard_page: DashboardPage = sign_in_page.run_login(
+            username='admin@mail.com',
+            password='Test@12345'
+        )
+
+        # Assert properties of the dashboard page
+        # i.e. (page title, labels, and text buttons)
+        try:
+            dashboard_page.assert_properties()
+        except ValueError as err:
+            self.fail(f'Dashboard Page Assertion Failed: {str(err)}')
+
+        # Scrolls down/up to see dataset of the table employee
+        # (if needed)
+        if len(dashboard_page.table_dataset) >= 10:
+            dashboard_page.scroll_down()
+            dashboard_page.scroll_up()
+
+        # Assert table headers
+        try:
+            dashboard_page.assert_table_headers()
+        except ValueError as err:
+            self.fail(f'Dashboard Page Assertion Failed: {str(err)}')
+
+        # Assert table content if empty
+        if len(dashboard_page.table_dataset) == 0:
+            try:
+                dashboard_page.assert_table_data_is_empty()
+            except ValueError as err:
+                self.fail(f'Dashboard Page Assertion Failed: {str(err)}')
+
+        # Assert dataset of the table employee
+        else:
+            actual_data = dashboard_page.table_dataset
+            expected_data = PREDEFINED_DATA
+            self.assertCountEqual(actual_data, expected_data)
+
+        # Logout from the system
+        sign_in_page = dashboard_page.run_logout()
+        time.sleep(1)
+
+        # Ensure sign page is loaded with correct properties
+        try:
+            sign_in_page.assert_properties()
+        except ValueError as err:
+            self.fail(f'Sign In Page Assertion Failed: {str(err)}')
 
     @classmethod
     def tearDownClass(cls):
