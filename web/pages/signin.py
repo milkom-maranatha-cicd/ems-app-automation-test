8e37f8e6df77927fd1cb1950eeea5006c2cb1af4
panjiyudasetya <panjiyudasetya@gmail.com>
2023-12-03 15:23:12 +0700

    Revamping sign in page

--- web/pages/signin.py
+++ web/pages/signin.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
 from web.driver import WebDriver
+from web.modals.basic import BasicModal
+from web.pages import DashboardPage
 from web.utils import visibility_of_element_located
 
 
@@ -11,78 +13,113 @@ class SignInPage:
     def __init__(self, wd: WebDriver) -> None:
         self.wd = wd
         self.driver = wd.driver
-        self.root_container = self.driver.find_element(By.ID, 'root')
 
-    def login(self, username: str, password: str):
+    @property
+    def container(self) -> WebElement:
+        """
+        Returns container of the sign in page.
+        """
+        return visibility_of_element_located(
+            self.driver, By.ID, 'root'
+        )
+
+    @property
+    def label_title(self) -> str:
+        """
+        Returns label of the title of the sign in page.
+        """
+        return self.container.find_element(
+            By.XPATH, '//*[@id="root"]/div/form/h1'
+        ).text
+
+    @property
+    def label_email(self) -> str:
+        """
+        Returns label of the email input field.
+        """
+        return self.container.find_element(
+            By.XPATH, '//*[@id="root"]/div/form/label[1]'
+        ).text
+
+    @property
+    def input_email(self) -> WebElement:
+        """
+        Returns email input field.
+        """
+        return self.container.find_element(By.ID, 'email')
+
+    @property
+    def label_password(self) -> str:
+        """
+        Returns label of the password input field.
+        """
+        return self.container.find_element(
+            By.XPATH, '//*[@id="root"]/div/form/label[2]'
+        ).text
+
+    @property
+    def input_password(self) -> WebElement:
+        """
+        Returns password input field.
+        """
+        return self.container.find_element(By.ID, 'password')
+
+    @property
+    def label_btn_login(self) -> str:
+        """
+        Returns label of the button login.
+        """
+        return self.btn_login.get_attribute('value')
+
+    @property
+    def btn_login(self) -> WebElement:
+        """
+        Returns button login.
+        """
+        return self.container.find_element(
+            By.XPATH, '//*[@id="root"]/div/form/input[3]'
+        )
+
+    def assert_properties(self) -> None:
+        """
+        Assert sign in page properties.
+        """
+        if self.label_title != 'Admin Login':
+            raise ValueError("Incorrect page's title.")
+
+        if self.label_email != 'Email':
+            raise ValueError('Incorrect label for the email input field.')
+
+        if self.label_password != 'Password':
+            raise ValueError('Incorrect label for the password input field.')
+
+        if self.label_btn_login != 'Login':
+            raise ValueError('Incorrect label for the login button.')
+
+    def run_login(self, username: str, password: str) -> DashboardPage:
         """
         Login to the Employee Management System app
         with valid credential.
         """
-        # Login functionality
-        input_email = self.get_input_email()
-        input_email.send_keys(username)
+        # Login scenario
+        self.input_email.send_keys(username)
+        self.input_password.send_keys(password)
+        self.btn_login.click()
 
-        input_password = self.get_input_password()
-        input_password.send_keys(password)
-
-        btn_login = self.get_btn_login()
-        btn_login.click()
-
-        # Assert response message
-        message = visibility_of_element_located(
-            self.driver, By.ID, 'swal2-title'
-        )
-
-        if message.text != 'Successfully logged in!':
-            print('Login failed...')
-            self.driver.close()
-
-        print('Login success...')
+        # Check response message
+        if not self._is_login_success():
+            raise PermissionError(
+                'Unable to open dashboard page. Login failed!'
+            )
 
         # Redirect to dashboard page
-        from web.pages import DashboardPage
         return DashboardPage(self.wd)
 
-    def get_label_title(self) -> str:
+    def _is_login_success(self):
         """
-        Returns the title of the sign in page.
+        Returns `True` if login is success.
         """
-        return self.root_container.find_element(
-            By.XPATH, '//*[@id="root"]/div/form/h1'
-        ).text
-
-    def get_label_email(self) -> str:
-        """
-        Returns the label of email input field.
-        """
-        return self.root_container.find_element(
-            By.XPATH, '//*[@id="root"]/div/form/label[1]'
-        ).text
-
-    def get_input_email(self) -> WebElement:
-        """
-        Returns email input field.
-        """
-        return self.root_container.find_element(By.ID, 'email')
-
-    def get_label_password(self):
-        """
-        Returns the label of password input field.
-        """
-        return self.root_container.find_element(
-            By.XPATH, '//*[@id="root"]/div/form/label[2]'
-        ).text
-
-    def get_input_password(self) -> WebElement:
-        """
-        Returns password input field.
-        """
-        return self.root_container.find_element(By.ID, 'password')
-
-    def get_btn_login(self) -> WebElement:
-        """
-        Returns button login.
-        """
-        return self.root_container.find_element(
-            By.XPATH, '//*[@id="root"]/div/form/input[3]'
+        modal = BasicModal(self.wd)
+        return bool(
+            modal.label_title == 'Successfully logged in!'
         )
