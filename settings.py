e8023cf0030a0f32ea6c2393fbff2f4fa00e61af
panjiyudasetya <panjiyudasetya@gmail.com>
2024-02-02 15:52:09 +0700

    Add settings to enable headless browser

--- settings.py
+++ settings.py
@@ -1,4 +1,10 @@
+import os
+
 from enum import IntEnum
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 # Choices of the Automation Web Driver
@@ -13,3 +19,6 @@ WEB_DRIVER_TYPE = DriverType.CHROME
 
 # URL of the Single Page Application
 APP_URL = 'http://localhost:3000/'
+
+# Headless browser configuration
+USE_HEADLESS_BROWSER = os.environ.get('USE_HEADLESS_BROWSER', 'false').lower() in ['true', 'yes', '1']
